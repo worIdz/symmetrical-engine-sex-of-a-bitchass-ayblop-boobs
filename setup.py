12c3ef276110042ce3c336e23111559964a5c786
worIdz <96669877+worIdz@users.noreply.github.com>
2021-12-25 18:25:32 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+
+setuptools.setup(
+name="Api_Wrapper", #Replace with your own package name
+version="0.0.1",
+author = "veiyo",
+description="ROBLOX API Wrapper",
+packages=["Api_Wrapper"]
+)
